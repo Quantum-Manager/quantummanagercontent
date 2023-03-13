995877386e37e67f20b77b20a3a580c8f38c4fe1
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2023-03-13 12:40:40 +0300

    fix

--- quantummanagercontent.php
+++ quantummanagercontent.php
@@ -29,10 +29,10 @@ class PlgContentQuantummanagercontent extends CMSPlugin
 
 
 	/**
-	 * @param $context
-	 * @param $item
-	 * @param $params
-	 * @param int $page
+	 * @param        $context
+	 * @param        $item
+	 * @param        $params
+	 * @param   int  $page
 	 *
 	 *
 	 * @since version
@@ -66,44 +66,49 @@ class PlgContentQuantummanagercontent extends CMSPlugin
 	private function prepare($string, $context, $item)
 	{
 
-		if(strpos($string, '[qmcontent]') === false)
+		if (!is_string($string))
+		{
+			return $string;
+		}
+
+		if (strpos($string, '[qmcontent]') === false)
 		{
 			return $string;
 		}
 
 		JLoader::register('QuantummanagerbuttonHelper', JPATH_ROOT . '/plugins/editors-xtd/quantummanagerbutton/helper.php');
-		$regex = "/\[qmcontent\](.*?)\[\/qmcontent\]/i";
+		$regex  = "/\[qmcontent\](.*?)\[\/qmcontent\]/i";
 		$string = preg_replace_callback($regex, static function ($matches) {
-			$output = '';
-			$content = &$matches[1];
-			$before = '';
+			$output    = '';
+			$content   = &$matches[1];
+			$before    = '';
 			$variables = '';
-			$item = '';
-			$after = '';
+			$item      = '';
+			$after     = '';
 
 			preg_replace_callback("/\[before\](.*?)\[\/before\]/i", function ($matchesBefore) use (&$before, &$output) {
-			    if(preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
-                {
-                    $before = str_replace(['{', '}'], '', $matchesBefore[1]);
-                    $output .= QuantummanagerbuttonHelper::renderLayout($before);
-                }
-			    else
-                {
-                    $output .= $before;
-                }
+				if (preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
+				{
+					$before = str_replace(['{', '}'], '', $matchesBefore[1]);
+					$output .= QuantummanagerbuttonHelper::renderLayout($before);
+				}
+				else
+				{
+					$output .= $before;
+				}
 
 			}, $content);
 
 			preg_replace_callback("/\[template\](.*?)\[\/template\]/i", function ($matchesBefore) use (&$item) {
-                if(preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
-                {
-                    $item = str_replace(['{', '}'], '', $matchesBefore[1]);
-                    $item = QuantummanagerbuttonHelper::renderLayout($item);
-                }
+				if (preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
+				{
+					$item = str_replace(['{', '}'], '', $matchesBefore[1]);
+					$item = QuantummanagerbuttonHelper::renderLayout($item);
+				}
 				else
-                {
-                    $item = $matchesBefore[1];
-                }
+				{
+					$item = $matchesBefore[1];
+				}
 			}, $content);
 
 			preg_replace_callback("/\[variables\](.*?)\[\/variables\]/i", function ($matchesBefore) use (&$variables) {
@@ -111,46 +116,46 @@ class PlgContentQuantummanagercontent extends CMSPlugin
 			}, $content);
 
 
-			if(!empty($variables) && !empty($item))
+			if (!empty($variables) && !empty($item))
 			{
 
 				$variables = json_decode($variables, JSON_OBJECT_AS_ARRAY);
 
-				if(is_array($variables) && count($variables) > 0)
+				if (is_array($variables) && count($variables) > 0)
 				{
 					foreach ($variables as $variable)
 					{
-                        $outputItem = $item;
-                        $variablesFind = [];
+						$outputItem       = $item;
+						$variablesFind    = [];
 						$variablesReplace = [];
 
 						foreach ($variable as $key => $value)
 						{
-							$variablesFind[] = $key;
+							$variablesFind[]    = $key;
 							$variablesReplace[] = $value;
 						}
 
 						$outputItem = str_replace($variablesFind, $variablesReplace, $outputItem);
 						$outputItem = preg_replace("#[a-zA-Z]{1,}\=\"\"#isu", '', $outputItem);
-						$output .= $outputItem;
+						$output     .= $outputItem;
 					}
 				}
 
 
 			}
 
-            preg_replace_callback("/\[after\](.*?)\[\/after\]/i", function ($matchesBefore) use (&$after, &$output) {
-                if(preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
-                {
-                    $after = str_replace(['{', '}'], '', $matchesBefore[1]);
-                    $output .= QuantummanagerbuttonHelper::renderLayout($after);
-                }
-                else
-                {
-                    $output .= $after;
-                }
+			preg_replace_callback("/\[after\](.*?)\[\/after\]/i", function ($matchesBefore) use (&$after, &$output) {
+				if (preg_match("#^\{\{.*?\}\}$#isu", $matchesBefore[1]))
+				{
+					$after  = str_replace(['{', '}'], '', $matchesBefore[1]);
+					$output .= QuantummanagerbuttonHelper::renderLayout($after);
+				}
+				else
+				{
+					$output .= $after;
+				}
 
-            }, $content);
+			}, $content);
 
 			return $output;
 		}, $string);
